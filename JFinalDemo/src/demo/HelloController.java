c6baa8ddb06782804a66606875476860de6a8e00
vimilai <2574097287@qq.com>
2016-05-09 11:17:15 +0800

    Update HelloController.java

--- JFinalDemo/src/demo/HelloController.java
+++ JFinalDemo/src/demo/HelloController.java
@@ -17,7 +17,7 @@ public class HelloController extends Controller {
 		System.out.println(users);
 		String v=getPara("v1");
 		System.out.println(v);
-		setAttr("sdsasasdasdsv", 1);
+		setAttr("v", 1);
 		renderJsp("/111.jsp");
 	}
 }
